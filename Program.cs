82d776236c7fe50d2305404f0e9f9810f8ac4f52
Ryan Lirio <ryanlirio2@gmail.com>
2024-10-02 16:31:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ListaDePresenca
+{
+    class Program
+    {
+        static void Main()
+        {
+            Name();
+        }
+
+        static void Name()
+        {
+            Console.WriteLine("Lista de presença de alunos");
+            Console.WriteLine("Alunos: ");
+
+            Queue<string> chamada = new Queue<string>();
+
+            chamada.Enqueue("Angelo");
+            chamada.Enqueue("Ganso");
+            chamada.Enqueue("Neilton");
+            chamada.Enqueue("Neymar");
+            chamada.Enqueue("Marcos Leonardo");
+            chamada.Enqueue("Robinho");
+            chamada.Enqueue("Vanderlei");
+            foreach (var obj in chamada)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("=================");
+            
+            
+            
+            for (int i = 0; i < chamada.Count; i++)
+            {   
+                string name1 = Console.ReadLine();
+                if(chamada.Contains(name1))
+                    Console.WriteLine("Presente\n----------");
+                else{
+                    Console.WriteLine("Faltou");
+                }
+            }
+
+
+        }
+
+        static void Comparar()
+        {
+            
+        }
+
+    }
+
+}
